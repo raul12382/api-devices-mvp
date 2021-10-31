478bc639c0df2baab9f11d7e0bd79a9de98f2a3d
raul12382 <raulwong325@gmail.com>
2021-10-31 12:37:26 -0600

    cambios mvp

--- src/database.js
+++ src/database.js
@@ -1,9 +1,12 @@
 import MongoClient from 'mongodb'
+import dotenv from 'dotenv'
+
+dotenv.config({path:'variables.env'})
 
 export async function connect() {
     try {
-        const client = await MongoClient.connect('mongodb://localhost:27017/', {useNewUrlParser: true});
-        const db = client.db('nodejs-restapi');
+        const client = await MongoClient.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+        const db = client.db('mvp');
         return db;
     } catch(e) {
         console.log(e);
